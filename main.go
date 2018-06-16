f92260879f5fd2eee035880657cb81fab5b241a5
Nehemiah Moses <nehemiah@servishero.com>
2018-06-16 15:50:40 +0200

    Handle POST requests by creating entries in Storm DB

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/asdine/storm"
 )
 
 func main() {
-	// r := mux.NewRouter()
 	http.HandleFunc("/restaurants", restaurants)
 	http.HandleFunc("/restaurants/", getone)
 
@@ -28,7 +30,7 @@ func restaurants(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		fmt.Fprintln(w, "get list of restaurants")
 	case "POST":
-		fmt.Fprintln(w, "create new restaurant")
+		create(w, r)
 	case "PUT":
 		fmt.Fprintln(w, "update existing restaurant")
 	case "DELETE":
@@ -38,18 +40,43 @@ func restaurants(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// func restaurantDetail(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "find restaurants wip")
-// }
-//
-// func createRestaurant(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "create restaurants wip")
-// }
-//
-// func updateRestaurant(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "update restaurants wip")
-// }
-//
-// func deleteRestaurant(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "delete restaurants wip")
-// }
+func create(w http.ResponseWriter, r *http.Request) {
+	var restaurant Restaurant
+	if err := json.NewDecoder(r.Body).Decode(&restaurant); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	db, err := storm.Open("my.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Save(&restaurant)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", restaurant)
+	respondWithJSON(w, http.StatusCreated, restaurant)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+type Restaurant struct {
+	ID          int `storm:"id,increment"` // primary key
+	Name        string
+	Phone       string
+	Cuisines    string // csv
+	Address     string
+	Description string
+}
